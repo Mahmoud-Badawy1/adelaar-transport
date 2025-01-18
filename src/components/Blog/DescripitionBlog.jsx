e0f26ae8b648004feed6253647d5eb8c379fa807
Mahmoud-Badawy1 <mahmoudbadawy10h@gmail.com>
2025-01-18 18:02:15 +0200

    done 5

--- src/components/Blog/DescripitionBlog.jsx
+++ src/components/Blog/DescripitionBlog.jsx
@@ -51,8 +51,8 @@ function DescripitionBlog() {
             </div>
 
             <div className="my-8">
-                <div className="font-light text-text1 mb-2">Door elke dag vanuit het perspectief van onze klanten te denken, anticiperen we op wat ze willen, bieden we aan wat ze nodig hebben en bouwen we duurzame relaties. Dit zijn de concepten die onze onderscheidende cultuur vormgeven en ons onderscheiden van anderen.</div>
-                <div className="font-light text-text1 mb-2">We begeleiden onze klanten door moeilijke kwesties, waarbij we onze inzichten en oordelen toepassen op elke situatie. Onze innovatieve benaderingen creëren originele oplossingen voor onze klanten.</div>
+                <div className="font-light text-text1 mb-2"> Door elke dag vanuit het perspectief van onze klanten te denken, anticiperen we op wat ze willen, bieden we aan wat ze nodig hebben en bouwen we duurzame relaties. Dit zijn de concepten die onze onderscheidende cultuur vormgeven en ons onderscheiden van anderen.</div>
+                <div className="font-light text-text1 mb-2"> We begeleiden onze klanten door moeilijke kwesties, waarbij we onze inzichten en oordelen toepassen op elke situatie. Onze innovatieve benaderingen creëren originele oplossingen voor onze klanten.</div>
             </div>
         </>
     )
