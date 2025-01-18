db952da3c11e1037ca182d49b0b8bfa486935b6b
Mahmoud-Badawy1 <mahmoudbadawy10h@gmail.com>
2025-01-18 18:06:27 +0200

    done 6

--- src/components/Services/oneService.jsx
+++ src/components/Services/oneService.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ServiceOptions from './serviceOption'   // src/components/Services/ServiceOptions.jsx
-import Image from 'next/image'
+import React from "react"
+import ServiceOptions from "./serviceOption"  // src/components/Services/ServiceOptions.jsx
+import Image from "next/image"
 function OneService() {
     return (
         <>
